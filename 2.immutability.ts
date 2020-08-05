56c947b8b01d7c2322c444e887e2ef5a939e971d
Sivabudh Umpudh <sivabudh@users.noreply.github.com>
2020-08-05 12:35:14 +0700

    Update 2.immutability.ts

--- 2.immutability.ts
+++ 2.immutability.ts
@@ -4,8 +4,10 @@ Question #2: Immutability
 The function below is a "safe divide" function. That is, it promises not to throw any "divide by zero" exceptions.
 
 Questions:
-1) Spot and describe the bug that causes the "divide by zero" to occur.
-2) Now, make a *single* code change such that the bug can be spotted by the compiler at *compile time*.
+1) Spot and describe the cause of the bug that causes the "divide by zero" to occur.
+2) Now, make a *single* code change on line 12:
+2.1) without changing the number 42;
+2.2) such that cause of the bug can be spotted by the compiler at *compile time*.
 3) What did you learn from this exercise?
 */
 function safeDivide(): number {
